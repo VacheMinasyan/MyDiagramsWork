2f377f538129a4c2248a89c8e85516b4c57c4d06
VacheMinasyan <vache.minasyan96gmail.comgit config --global user.email vache.minasyan96gmail.com>
2021-05-23 11:56:28 +0400

    added canvas diagram part4

--- src/js/script.js
+++ src/js/script.js
@@ -43,6 +43,98 @@ var myChart = new Chart(ctx, {
 
     }
 });
+// ======================= chart line diagrms ====================
+
+
+var ctx = document.getElementById('myChart2').getContext('2d');
+Chart.defaults.scale.gridLines.display = false;
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["01", "02", "03", "04", "05", "06", "07"],
+        datasets: [{
+            label: false,
+            data: [200, 400, 250,100,500,250, 250,250],
+            backgroundColor: 'transparent',
+            borderColor: ["rgb(67, 57, 242)"],
+            borderWidth: 3,
+            radius: 0,
+            lineTension: 0,
+            fill: false
+        },
+        {
+            label: false,
+            data: [250,350,450,550, 425,300, 550,550],
+            backgroundColor: 'transparent',
+            borderColor: ["rgb(255, 58, 41)"],
+            borderWidth: 3,
+            radius:0,
+            lineTension: 0,
+            fill: false
+
+        },
+       
+        {
+            label: false,
+            data: [0, 100, -200, 200,450,450,450,450],
+            backgroundColor: 'transparent',
+            borderColor: ["rgb(255, 178, 0)"],
+            borderWidth: 3,
+            lineTension: 0,
+            radius: 0,
+            fill: false
+
+        },
+         {
+            label: false,
+            data: [50, -250,-25,200,-250,200,100,250],
+            backgroundColor: 'transparent',
+            borderColor: ["rgb(52, 181, 58)"],
+            borderWidth: 3,
+            lineTension: 0,
+            radius: 0,
+        },
+
+        ]
+    },
+    options: {
+        plugins: {
+            datalabels: {
+
+            },
+        },
+        legend: {
+            display: false
+        },
+        tooltips: {
+        },
+        scales: {
+            xAxes: [
+                {
+                    ticks: {
+                        // display: false,
+                        beginAtZero: true,
+
+                    }
+                }],
+            yAxes: [{
+                ticks: {
+                    display: false,
+                    min: -300,
+                    max: 600,
+                }
+            }]
+
+        }
+
+    }
+});
+
+
+
+
+
+
 
 
 
